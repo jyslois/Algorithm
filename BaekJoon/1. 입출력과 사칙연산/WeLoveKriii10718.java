46dcbc2aeda46ee91961359cb48d010766df72ed
VisionPG <jyseolois@naver.com>
2022-06-26 15:13:15 +0900

    no message

--- BaekJoon/1. 입출력과 사칙연산/WeLoveKriii10718.java
+++
@@ -1,8 +0,0 @@
-class WeLoveKriii10718 {
-    public static void main(String[] args) {
-        String sen = "강한친구 대한육군";
-        for (int i = 0; i < 2; i++) {
-            System.out.println(sen);
-        }
-    }
-}
