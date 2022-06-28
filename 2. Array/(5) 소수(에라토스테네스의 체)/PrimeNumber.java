159f56d06e59f6b4367e691390bb26cf4e7736b8
VisionPG <jyseolois@naver.com>
2022-06-28 18:41:10 +0900

    no message

--- 2. Array/(5) 소수(에라토스테네스의 체)/PrimeNumber.java
+++ 2. Array/(5) 소수(에라토스테네스의 체)/PrimeNumber.java
@@ -16,6 +16,19 @@ import java.io.InputStreamReader;
 위의 과정을 반복하면 구하는 구간의 모든 소수가 남는다.
  */
 
+/*
+설명
+자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.
+만약 20이 입력되면 1부터 20까지의 소수는
+2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다.
+
+입력
+첫 줄에 자연수의 개수 N(2<=N<=200,000)이 주어집니다.
+
+출력
+첫 줄에 소수의 개수를 출력합니다.
+ */
+
 public class PrimeNumber {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
